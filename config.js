01d36067fe4bdd731e375964e5dc826aaaf4e581
Riswanganz <100130164+Riswanganz@users.noreply.github.com>
2023-01-03 20:50:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -47,27 +47,27 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/rellz.fvnkyy' //Link Instagrammu
-global.sgh = 'https://github.com/rellzxd' //Link Githubmu
-global.sgc = 'https://chat.whatsapp.com/IqFfoXcdh7UFyPoQTpKoQp'
+global.sig = 'https://instagram.com/rwnzco' //Link Instagrammu
+global.sgh = 'https://github.com/Riswanganz' //Link Githubmu
+global.sgc = 'https://chat.whatsapp.com/LBtXtWtr50NLugyVjt1hZf'
 global.sdc = '-' //Isi Pake Link Discordmu Kalo Gada Biarin aja
-global.snh = 'https://nhentai.net/g/HaramTod🗿' //Hmmm, Ini boleh kalian isi terserah :v
+global.snh = 'https://bandarrandom.blogspot.com' //Hmmm, Ini boleh kalian isi terserah :v
 
 /*============== PAYMENT ==============*/
-global.pdana = '087833306677' //Nomor Danamu
-global.ppulsa = '087833306677' //Xl
-global.psaweria = 'https://saweria.com/RellzFvnkyy' //Link Saweriamu Kalo Nggada Ketik - aja
+global.pdana = '083804343232' //Nomor Danamu
+global.ppulsa = '083804343232' //Xl
+global.psaweria = 'https://saweria.co/Kakekmeme' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6285876138954' //Nomor Bot
-global.nomorown = '84352438984' //Nomor Owner
-global.namebot = 'Fvnkyy BOTz' //Nama Bot
-global.nameown = 'Rellz' // Nama Owner
+global.nomorbot = '6283804343232' //Nomor Bot
+global.nomorown = '83804343232' //Nomor Owner
+global.namebot = 'Cyber BOTz' //Nama Bot
+global.nameown = 'Rizwan' // Nama Owner
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['84352438984', '❦ rellz 🎐', true] //Ganti jd nomormu sama Namamu
+  ['83804343232', '❦ rizwan 🎐', true] //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -99,12 +99,12 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 FVNKYY BOTZ 汉  」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘FVNKYY BOTZ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = 'FVNKYY BOTZ'
+global.wm = '                「 Cyber BOTZ 汉  」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Cyber BOTZ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = 'Cyber BOTZ'
 global.botdate = `⫹⫺ 𝗛𝗮𝗿𝗶: ${week} ${date}`
 global.bottime = `𝗪𝗮𝗸𝘁𝘂 : ${wktuwib}`
-global.titlebot = 'By Rellz'
+global.titlebot = 'By Rizwan'
 global.author = global.wm
 global.logo = 'https://ibb.co/17RY7x9'
 
@@ -330,7 +330,7 @@ global.wait = '```「▰▰▰▱▱▱▱▱▱▱」Loading...```'
 global.eror = '```404 error.```'
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 857-3617-8354'
+global.phn = '+62 838-0434-3232'
 
 /*=========== TYPE DOCUMENT ===========*/
 global.dpptx = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -371,9 +371,9 @@ global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
 global.stickpack = '.'
-global.stickauth = `☂︎\nF\nV\nN\nK\nY\n\nY\n-\n𝗕\n𝗢\n𝗧\n✦\n\n⫹⫺ Whatsapp BOT\nwa.me/${global.nomorbot}`
-global.packname = 'By Rellz'
-global.packname2 = 'Ig : rellz.fvnkyy'
+global.stickauth = `☂︎\nC\nY\nB\nE\nR\n-\n𝗕\n𝗢\n𝗧\n✦\n\n⫹⫺ Whatsapp BOT\nwa.me/${global.nomorbot}`
+global.packname = 'By Rizwan'
+global.packname2 = 'Ig : rwnzco'
 
 global.multiplier = 38 // The higher, The harder levelup
 
